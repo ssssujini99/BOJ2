65198387176fe7f3a7552931071f1bf348465f4f
ssssujini99 <sssssjin99@naver.com>
2023-01-05 17:38:18 +0900

    :sparkles: Update(1715) - 카드 정렬하기 / Greedy

---
+++ Greedy/1715/sol1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <cstring>
+#include <set>
+#include <climits>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N; ll tmp;
+    priority_queue<ll, vector<ll>, greater<> > pq;
+
+    cin >> N;
+    for(int i=0; i<N; i++) {
+        cin >> tmp;
+        pq.push(tmp);
+    }
+
+    ll t1, t2, ans = 0;
+    while(pq.size() != 1) {
+        t1 = pq.top(); pq.pop();
+        t2 = pq.top(); pq.pop();
+
+        pq.push(t1+t2);
+        ans += (t1+t2);
+    }
+
+    cout << ans << endl;
+    return 0;
+}
